bec677316e5925ea9c84e70f659379b35bbabb84
czasg <972542655@qq.com>
2024-01-07 20:43:45 +0800

    feat: update

--- clear.go
+++ clear.go
@@ -16,11 +16,11 @@ func panicTodo(file string) error {
 	}
 
 	// 使用正则表达式匹配以 func 开始的函数，替换函数体为 panic("TODO")
-	re := regexp.MustCompile(`func (\w+)\s*\([^\)]*\)\s*\{[^\}]*\}`)
-	newContent := re.ReplaceAllString(string(content), `func $1() { panic("TODO") }`)
-
+	re := regexp.MustCompile(`(\n//[\s\S]*?func.*?{)[\s\S]*?\n}`)
+	newContent := re.ReplaceAllString(string(content), "$1\n\t panic(\"TODO???\")\n}")
+	fmt.Println(newContent)
 	// 写入替换后的内容到文件
-	err = ioutil.WriteFile(file, []byte(newContent), os.ModePerm)
+	err = os.WriteFile(file, []byte(newContent), os.ModePerm)
 	if err != nil {
 		return err
 	}
@@ -28,12 +28,16 @@ func panicTodo(file string) error {
 	return nil
 }
 
+// mm
 func processGoFiles(root string) error {
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if !info.IsDir() && strings.HasSuffix(info.Name(), ".go") && !strings.HasSuffix(info.Name(), "_test.go") {
+			if info.Name() == "clear.go" {
+				return nil
+			}
 			fmt.Printf("Processing: %s\n", path)
 			err := panicTodo(path)
 			if err != nil {
