ee52a9935fca0188e0c2b235e039aa39cad633d8
czasg <972542655@qq.com>
2024-01-08 09:54:36 +0800

    feat: update

--- sort/sort.go
+++ sort/sort.go
@@ -2,12 +2,30 @@ package sort
 
 // 冒泡排序
 func BubbleSort(arr []int) []int {
-	panic("TODO")
+	n := len(arr)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	return arr
 }
 
 // 选择排序
 func SelectionSort(arr []int) []int {
-	panic("TODO")
+	n := len(arr)
+	for i := 0; i < n; i++ {
+		selectIndex := i
+		for j := selectIndex; j < n; j++ {
+			if arr[j] < arr[selectIndex] {
+				selectIndex = j
+			}
+		}
+		arr[i], arr[selectIndex] = arr[selectIndex], arr[i]
+	}
+	return arr
 }
 
 // 插入排序
