22003078fdfcc267ea4207d111288b5c91317fb9
陈子昂 <chenziang@antiy.cn>
2024-01-08 11:01:34 +0800

    feat: update

--- math/math.go
+++ math/math.go
@@ -1,16 +1,39 @@
 package math
 
 // 最大值
-func Max(values ...int) int {
-	panic("TODO")
+func Max(a, b int, others ...int) (ans int) {
+	if a > b {
+		ans = a
+	} else {
+		ans = b
+	}
+	for i := 0; i < len(others); i++ {
+		if others[i] > ans {
+			ans = others[i]
+		}
+	}
+	return
 }
 
 // 最小值
-func Min(values ...int) int {
-	panic("TODO")
+func Min(a, b int, others ...int) (ans int) {
+	if a < b {
+		ans = a
+	} else {
+		ans = b
+	}
+	for i := 0; i < len(others); i++ {
+		if others[i] < ans {
+			ans = others[i]
+		}
+	}
+	return
 }
 
 // 绝对值
 func Abs(value int) int {
-	panic("TODO")
+	if value < 0 {
+		return -value
+	}
+	return value
 }
