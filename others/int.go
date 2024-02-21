2635bc21168215a2000bf41c104b8a92f9c47af8
陈子昂 <chenziang@antiy.cn>
2024-02-21 17:00:30 +0800

    feat: update

--- others/int.go
+++ others/int.go
@@ -1,16 +1,10 @@
 package others
 
-import "math"
-
 // 整数反转
 func reverseInt(x int) int {
 	ans := 0
-	for x != 0 {
-		if ans > math.MaxInt32/10 || ans < math.MinInt32/10 {
-			return 0
-		}
-		pop := x % 10
-		ans = ans*10 + pop
+	for x > 0 {
+		ans = ans*10 + x%10
 		x /= 10
 	}
 	return ans
