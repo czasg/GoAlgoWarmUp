0cd5ed210134f96509b011112bd20538588e2722
陈子昂 <chenziang@antiy.cn>
2024-03-06 17:43:30 +0800

    feat: update

--- array/array.go
+++ array/array.go
@@ -60,15 +60,15 @@ func removeElement(nums []int, val int) int {
 
 // 搜索插入位置 - 二分查找法
 func searchInsert(nums []int, target int) int {
-	left, right := 0, len(nums)-1
-	for left <= right {
+	left, right := 0, len(nums)
+	for left < right {
 		mid := (right + left) / 2
 		if nums[mid] == target {
 			return mid
 		} else if nums[mid] < target {
 			left = mid + 1
 		} else {
-			right = mid - 1
+			right = mid
 		}
 	}
 	return left
