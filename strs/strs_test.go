b173a6921a88afe729903bf060bdcc53cc72dd4c
陈子昂 <chenziang@antiy.cn>
2024-03-14 17:59:08 +0800

    feat: update

--- strs/strs_test.go
+++ strs/strs_test.go
@@ -34,3 +34,43 @@ func Test_longestCommonPrefix(t *testing.T) {
 		})
 	}
 }
+
+func Test_longestPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "",
+			args: args{
+				s: "babba",
+			},
+			want: "abba",
+		},
+		{
+			name: "",
+			args: args{
+				s: "bbbbad",
+			},
+			want: "bbbb",
+		},
+		{
+			name: "",
+			args: args{
+				s: "abaccddffgg",
+			},
+			want: "aba",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
