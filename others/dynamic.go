1e5f46490a829ffda444712e7fc7752ed52885ba
陈子昂 <chenziang@antiy.cn>
2024-03-14 14:32:55 +0800

    feat: update

--- others/dynamic.go
+++ others/dynamic.go
@@ -2,60 +2,52 @@ package others
 
 // 最大子数组和 - 动态规划
 func maxSubArray(nums []int) int {
-	if len(nums) < 1 {
-		return 0
-	}
+	curSum := nums[0]
 	maxSum := nums[0]
 	for i := 1; i < len(nums); i++ {
-		curSum := nums[i]
-		if maxSum+curSum > curSum {
-			curSum = maxSum + curSum
-		}
+		curSum = max(nums[i], nums[i]+curSum)
 		if curSum > maxSum {
 			maxSum = curSum
 		}
 	}
 	return maxSum
-	//if len(nums) == 0 {
-	//	return 0
-	//}
-	//maxSum := nums[0]
-	//currentSum := nums[0]
-	//for i := 1; i < len(nums); i++ {
-	//	// 在当前元素加入之前的子数组和加上当前元素的值进行比较
-	//	currentSum = int(math.Max(nums[i], currentSum+nums[i]))
-	//	// 更新全局最大和
-	//	maxSum = int(math.Max(maxSum, currentSum))
-	//}
-	//return maxSum
 }
 
 // 爬楼梯-动态规划
 func climbStairs(n int) int {
-	if n <= 1 {
+	if n < 2 {
 		return 1
 	}
-	arr := make([]int, n+1)
-	arr[0], arr[1] = 1, 1
-	for i := 2; i <= n; i++ {
+	arr := make([]int, n)
+	arr[0] = 1
+	arr[1] = 2
+	for i := 2; i < n; i++ {
 		arr[i] = arr[i-1] + arr[i-2]
 	}
-	return arr[n]
+	return arr[n-1]
 }
 
 // 打家劫舍 - 不能偷取相邻的数值
 func rob(nums []int) int {
-	length := len(nums)
-	if length == 0 {
+	n := len(nums)
+	if n < 1 {
 		return 0
-	} else if length == 1 {
+	}
+	if n == 1 {
 		return nums[0]
 	}
-	dp := make([]int, length)
-	dp[0] = nums[0]
-	dp[1] = max(nums[0], nums[1])
-	for i := 2; i < length; i++ {
-		dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+	arr := make([]int, n)
+	arr[0] = nums[0]
+	arr[1] = max(nums[0], nums[1])
+	for i := 2; i < n; i++ {
+		arr[i] = max(arr[i-1], nums[i]+arr[i-2])
 	}
-	return dp[length-1]
+	return arr[n-1]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
--- others/dynamic_test.go
+++ others/dynamic_test.go
@@ -16,13 +16,100 @@ func Test_rob(t *testing.T) {
 			args: args{
 				nums: []int{1, 2, 3, 1},
 			},
-			want: 4,
+			want: 4, // 1+3
+		},
+		{
+			name: "",
+			args: args{
+				nums: []int{2, 7, 9, 3, 1},
+			},
+			want: 12, // 2+9+1
+		},
+		{
+			name: "",
+			args: args{
+				nums: []int{10, 2, 2, 10},
+			},
+			want: 20, // 10+10
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := rob(tt.args.nums); got != tt.want {
 				t.Errorf("rob() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_maxSubArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				nums: []int{-2, 1, -3, 4, -1, 2, 1, -5, 4},
+			},
+			want: 6, // 4, -1, 2, 1
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxSubArray(tt.args.nums); got != tt.want {
+				t.Errorf("maxSubArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_climbStairs(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				n: 1,
+			},
+			want: 1,
+		},
+		{
+			name: "",
+			args: args{
+				n: 2,
+			},
+			want: 2,
+		},
+		{
+			name: "",
+			args: args{
+				n: 3,
+			},
+			want: 3,
+		},
+		{
+			name: "",
+			args: args{
+				n: 4,
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := climbStairs(tt.args.n); got != tt.want {
+				t.Errorf("climbStairs() = %v, want %v", got, tt.want)
 			}
 		})
 	}
